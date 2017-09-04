33ee46330a7a9b39085d4e1f0fa312b7435b1d06
dustinpfister <dustin.pfister@gmail.com>
2017-09-04 14:26:53 -0400

    mf_game1.js is now just a draw method, and a main loop

--- mf_game1.js
+++ mf_game1.js
@@ -1,118 +1,9 @@
 
-// player ships
-var ps,
-es,
-currentPl = {};
-//lastFire = new Date();
 
-// my "on planet" method
-var onPl = function (sec, obj) {
 
-    var i;
-    if (sec) {
-
-        if (sec.pl) {
-
-            i = sec.pl.length;
-            while (i--) {
-
-                if (_.b(sec.pl[i], obj)) {
-
-                    return sec.pl[i];
-
-                }
-
-            }
-
-        }
-
-    }
-
-    return false;
-
-};
-
-var init = function () {
-
-    _.l('init...');
-
-    // view port
-    vp.nw = 640;
-    vp.nh = 480;
-    vp.zoom(1);
-
-    // canvas
-    C.canvas.width = 640;
-    C.canvas.height = 480;
-    C.cls();
-
-    // sections
-    S.set(640, 480, 10, 10);
-    S.ls(vp.x, vp.y, vp.w, vp.h);
-
-    // pl array for each section
-    S.secs.forEach(function (sec) {
-
-        sec.pl = [];
-
-    });
-
-    // some starting planets
-    var sec = S.getPos(-32, -32);
-    sec.pl.push({
-
-        x : -32,
-        y : -32,
-        w : 64,
-        h : 64,
-        id : 'home',
-        po : false
-
-    });
-    sec.pl.push({
-
-        x : -230,
-        y : -128,
-        w : 32,
-        h : 32,
-        id : 'moon',
-        po : false
-
-    });
-
-    // the New Player Ship Collection that will replace playerObj, and pShots
-    ps = new ShipCollection({
-            faction : 'p',
-            //ai : true,
-            max : 1
-        });
-
-    // add the single player ship
-    ps.addShip();
-
-    es = new ShipCollection({
-            faction : 'e',
-            ai : true,
-            max : 5
-        });
-
-    // set enemy collections for each collection
-    ps.enemys = es;
-    es.enemys = ps;
-
-    es.addShip({
-
-        x : 200
-
-    });
-
-    _.l(ps);
-    _.l(es);
-
-},
 
 // draw method
-draw = function () {
+var draw = function () {
 
     var x,
     obj,
@@ -228,87 +119,9 @@ draw = function () {
 
 },
 
-x = 0, y = 0, i = 0, max = 1250,
 loop = function () {
 
     requestAnimationFrame(loop);
-
-    // player object
-    var obj = ps.units[0];
-
-    var d = kc.d();
-
-    if (d >= 0) {
-
-        // new _.asd method works great
-        if (_.asd(obj.a, d) == -1) {
-
-            obj.a -= Math.PI / 100;
-
-        } else {
-
-            obj.a += Math.PI / 100;
-        }
-
-        obj.step();
-    }
-
-    vp.lookAt(x, y);
-
-    S.ls(vp.x, vp.y, vp.w, vp.h);
-
-    vp.x = obj.x - vp.w / 2;
-    vp.y = obj.y - vp.h / 2;
-
-    // fire shots
-    if (kc.keys[186]) {
-
-        obj.shoot();
-
-    }
-
-    if (kc.keys[49]) {
-
-        ps.ai = true;
-
-    }
-
-    if (kc.keys[50]) {
-
-        ps.ai = false;
-
-    }
-
-    // get planet
-    var pl = onPl(S.getPos(obj.x, obj.y), obj);
-
-    currentPl = {};
-    if (pl) {
-
-        currentPl = pl;
-
-    }
-
-    ps.update();
-    es.update();
-
-    draw();
-
-    i += 1;
-    if (i === max) {
-
-        i = 0;
-
-    }
-
-};
-
-_.l('wtf');
-_.l(rw);
-
-var loop2 = function () {
-
-    requestAnimationFrame(loop2);
 
     rw.tick();
     draw();
@@ -316,10 +129,4 @@ var loop2 = function () {
 };
 
 rw.init();
-
-_.l('cut it out');
-_.l(rw.ps);
-
-//init();
-//loop();
-loop2();
+loop();
